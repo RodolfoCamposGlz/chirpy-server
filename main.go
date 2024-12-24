7ae45a9c231d413e88cd46ba316873415fc427b0
RodolfoCamposGlz <rodolfocampos459@gmail.com>
2024-12-24 16:48:02 -0600

    Added middleware counter and metrics handlers

--- main.go
+++ main.go
@@ -1,31 +1,69 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
 const port = "8080"
 const filepathRoot = "."
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	// Return an http.Handler that wraps the next handler
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Increment your metric
+		cfg.fileserverHits.Add(1)
+
+		// Call the next handler in the chain
+		next.ServeHTTP(w, r)
+	})
+}
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())))
+}
+
+func (cfg *apiConfig) handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+var handler http.Handler = http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
+
 func main (){
 	mux := http.NewServeMux()
-
+	apiCfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 	// Create a new http.Server
 	server := &http.Server{
 		Addr:  ":" + port, // Bind to port 8080
 		Handler: mux,     // Use the ServeMux as the handler
 	}
 
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot))))
-
-	mux.HandleFunc("/healthz",  func(w http.ResponseWriter, req *http.Request){
-		w.Header().Set("Content-Type","text/plain; charset=utf-8")
-		w.WriteHeader(200)
-		w.Write([]byte("OK"))
-	})
+	mux.Handle("/app/",apiCfg.middlewareMetricsInc(handler))
+	mux.HandleFunc("/healthz", apiCfg.handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	// Start the server
 	log.Printf("Serving files from %s on port: %s\n", filepathRoot, port)
 	log.Fatal(server.ListenAndServe())
 }
+
+
+
